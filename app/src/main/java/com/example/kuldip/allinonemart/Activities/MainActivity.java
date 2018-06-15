70ba3b4f6cfb6be8427fc38b9aa42f049d208ae9
imkuldip <imkuldip10@gmail.com>
2018-06-15 13:59:09 +0545

    commit after intial commit

--- app/src/main/java/com/example/kuldip/allinonemart/Activities/MainActivity.java
+++ app/src/main/java/com/example/kuldip/allinonemart/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.kuldip.allinonemart;
+package com.example.kuldip.allinonemart.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.kuldip.allinonemart.R;
 
 public class MainActivity extends AppCompatActivity {
 
